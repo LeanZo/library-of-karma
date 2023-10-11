c5fb057015782c244f9e74d3e2c1686c6ebe3ac4
LeanZo <kashchannel@gmail.com>
2023-10-10 22:50:07 -0300

    (。_。) Tenta adiciona sitemap e analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,38 @@ module.exports = {
         icon: `src/images/icon.png`,
       },
     },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        serialize: ({ path, modifiedGmt }) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt,
+          }
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-HS264CCHM5", // Google Analytics / GA
+          "GTM-WC9WDCVJ", // Google Tag Manager
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Respect Do Not Track
+          respectDNT: true,
+        },
+      },
+    },
   ],
 }
